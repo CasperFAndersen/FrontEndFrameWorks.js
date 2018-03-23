54b1e1e5885ac62552e52bd206d3c53d3311eab5
Casper Froberg Andersen <casperfroberg@gmail.com>
2018-03-23 10:04:59 +0100

    Testing out Angular

--- compare-vue/src/components/Faq.vue
+++ compare-vue/src/components/Faq.vue
@@ -3,10 +3,10 @@
     <div class="container">
       <section class="section">
         <h1 class="title">FAQ</h1>
-        <h2 class="subtitle">Lorum ipsum and all of that jazz.</h2>
+        <h2 class="subtitle is-4">Lorum ipsum and all of that jazz.</h2>
 
         <div class="columns" v-if="faqs && faqs.length">
-            <div class="column is-one-third" v-for="faq of faqs">
+            <div class="column is-one-third" v-for="faq of faqs" :key="faq.id">
               <div class="card">
                 <div class="card-content">
                   <p class="title">{{faq.title}}</p>
